20b3d97b2d18937f0671f97054f71e20e68acccb
Resul <garnnet_221@hotmail.com>
2022-04-29 14:46:39 +0300

    First Commit

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Grid
+{
+    public int width;
+    public int height;
+    public float cellSize;
+    public Transform gridStartTransform;
+    public List<Vector3> gridCenterPoses = new List<Vector3>();
+
+    private int[,] _gridArray;
+
+    public void CreateGrid()
+    {
+        _gridArray = new int[width, height];
+        
+        for (int x = 0; x < _gridArray.GetLength(0); x++)
+        {
+            //Debug.LogWarning("X = " + x);
+            for (int z = 0; z < _gridArray.GetLength(1); z++)
+            {
+                //Debug.LogWarning("Z = " + z);
+                
+                //Debug.DrawRay(GetCenterWorldPosition(x, z), Vector3.up * 20f, Color.red, 20f);
+                gridCenterPoses.Add(GetCenterWorldPosition(x, z));
+                
+                //Debug.DrawLine(GetWorldPosition(x, z), GetWorldPosition(x, z + 1), Color.green, 20f);
+                //Debug.DrawLine(GetWorldPosition(x, z), GetWorldPosition(x + 1, z), Color.green, 20f);
+            }
+        }
+        
+        //Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.green, 20f);
+        //Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.green, 20f);
+    }
+
+    public Vector3 GetCenterWorldPosition(float x, float z)
+    {
+        Vector3 worldPos = new Vector3(x, 0f, z);
+        worldPos *= cellSize;
+        worldPos.x += cellSize / 2f;
+        worldPos.z += cellSize / 2f;
+        worldPos.x += gridStartTransform.position.x;
+        worldPos.y += gridStartTransform.position.y;
+        worldPos.z += gridStartTransform.position.z;
+        
+        return worldPos;
+    }
+    
+    public Vector3 GetWorldPosition(float x, float z)
+    {
+        Vector3 worldPos = new Vector3(x, 0f, z);
+        worldPos *= cellSize;
+        worldPos.x += gridStartTransform.position.x;
+        worldPos.y += gridStartTransform.position.y;
+        worldPos.z += gridStartTransform.position.z;
+        
+        return worldPos;
+    }
+}
---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    public static GridController Instance { get; private set; }
+
+    public GameObject collectableJellyPrefab;
+    public Transform jelliesParent;
+    public List<GameObject> platforms = new List<GameObject>();
+    public List<Grid> grids = new List<Grid>();
+
+    
+    
+    private void Awake()
+    {
+        Instance = this;
+
+        for (int i = 0; i < platforms.Count; i++)
+            grids[i].CreateGrid();
+
+        for (int i = 0; i < grids[0].gridCenterPoses.Count; i++)
+            Instantiate(collectableJellyPrefab,grids[0].gridCenterPoses[i], Quaternion.identity, jelliesParent);
+    }
+    
+    //================================================================//
+    //===================Fisher_Yates_CardDeck_Shuffle====================//
+    //================================================================//
+ 
+    /// With the Fisher-Yates shuffle, first implemented on computers by Durstenfeld in 1964, 
+    ///   we randomly sort elements. This is an accurate, effective shuffling method for all array types.
+ 
+    public static void ShuffleList<T> (ref List<T> aList) {
+ 
+        System.Random _random = new System.Random ();
+ 
+        var myGO = default(T);
+ 
+        int n = aList.Count;
+        for (int i = 0; i < n; i++)
+        {
+            // NextDouble returns a random number between 0 and 1.
+            // ... It is equivalent to Math.random() in Java.
+            int r = i + (int)(_random.NextDouble() * (n - i));
+            myGO = aList[r];
+            aList[r] = aList[i];
+            aList[i] = myGO;
+        }
+ 
+        //return aList;
+    }
+}
